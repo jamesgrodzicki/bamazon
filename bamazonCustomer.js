88d3b3cd4ec2f3c23c963cde6ca6a37d3aa2e50a
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-08 20:10:51 -0500

    added prompt to show after table has been consoled

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const {table} = require('table');
+const { table } = require('table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -10,25 +10,46 @@ const connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
 });
 
-connection.query('SELECT * FROM products', function(err, result, fields){
-    if(err) throw err;
-    // console.log(result);
-    let resultArray = makeArray(result);
-    let output = table(resultArray);
-    console.log(output);
-});
+runCustomer();
 
 connection.end();
 
-function makeArray(arrayOfObjects){
+function runCustomer() {
+    connection.query('SELECT * FROM products', function (err, result, fields) {
+        if (err) throw err;
+        let resultArray = makeArray(result);
+        let output = table(resultArray);
+        console.log(output);
+        // let item_id = askID(result.length);
+        askID(result.length);
+    });
+}
+
+function askID(arrayLength) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Enter the ID of the item you would like to buy: '
+        }
+    ]).then(function (ans) {
+        if (ans.item_id > 0 && ans.item_id < arrayLength) {
+            return ans.item_id;
+        } else {
+            console.log(item_id + 'is not valid');
+        }
+    })
+}
+
+function makeArray(arrayOfObjects) {
     let keysArray = Object.keys(arrayOfObjects[0]);
     let newArray = [];
     newArray.push(keysArray);
-    for(let i=0; i<arrayOfObjects.length; i++){
+    for (let i = 0; i < arrayOfObjects.length; i++) {
         let arrayHolder = Object.values(arrayOfObjects[i]);
         newArray.push(arrayHolder);
     }
